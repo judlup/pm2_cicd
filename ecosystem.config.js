6809fa342c21cc55aa37606eff05f077548d064a
judlup <judlup@gmail.com>
2022-09-24 12:43:29 -0500

    Modified pm2 deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,35 @@ module.exports = {
     apps: [
         {
             name: "pm2-deploy",
-            script: "index.js"
+            script: "index.js",
+            env: {
+                "PORT": 5000,
+                "NODE_ENV":"dev"
+            },
+            env_prod: {
+                "PORT": 6000,
+                "NODE_ENV":"prod"
+            }
         }
     ],
     deploy: {
-        production: {
+        dev: {
             user: "judlup",
             host: "localhost",
             path: "/app",
             repo: "git@github.com:judlup/pm2_cicd.git",
             ref: "origin/main",
             key: "/home/judlup/.ssh/id_rsa.pub",
-            "post-deploy":"npm i; pm2 reload ecosystem.config.js --env production"
+            "post-deploy":"npm i; pm2 reload ecosystem.config.js --env dev"
+        },
+        prod: {
+            user: "judlup",
+            host: "localhost",
+            path: "/app",
+            repo: "git@github.com:judlup/pm2_cicd.git",
+            ref: "origin/main",
+            key: "/home/judlup/.ssh/id_rsa.pub",
+            "post-deploy":"npm i; pm2 reload ecosystem.config.js --env prod"
         }
     }
 }
